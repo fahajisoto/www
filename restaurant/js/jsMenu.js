1853404c1b00a2bea6a8f1a23420358c61fc4e2b
fatemeh <fatemeh.hajisotoudeh@gmail.com>
2013-06-07 17:20:53 +0200

    resto

--- restaurant/js/jsMenu.js
+++ restaurant/js/jsMenu.js
@@ -16,16 +16,15 @@ var counter=0;
 var Radresse;
 var Rcode_postal;
 var Rdescription;
+var nomResto;
 
 function init_itineraire(lat,lan){
 	end = new google.maps.LatLng(lat,lan);
 	getLocation();
-
 }
 
 function getLocation() 
 {
-	//alert("Loading, please wait...");
 	$("#mapholder").css({ opacity: 0, zoom: 0 });
 	if (navigator.geolocation)
 		{
@@ -136,7 +135,7 @@ function makeList(json) {
 		if( nbelt > 0 ) {		
 			for(i=0; i<nbelt;i++){
 				html +="<li class=\"ui-btn ui-btn-up-a ui-btn-icon-right ui-li-has-arrow ui-li ui-first-child\" data-corners=\"false\" data-shadow=\"false\" " +
-						"data-iconshadow=\"true\" onclick=\"menu("+i+");init_itineraire("+jsonResto[i].latitude+","+jsonResto[i].longitude+");makeaddress('"+jsonResto[i].adresse+"','"+jsonResto[i].code_postal+"','"+jsonResto[i].description+"')\" " +"data-wrapperels=\"div\" data-icon=\"arrow-r\" data-iconpos=\"right\">" +
+						"data-iconshadow=\"true\" onclick=\"makeaddress('"+jsonResto[i].nom+"','"+jsonResto[i].adresse+"','"+jsonResto[i].code_postal+"','"+jsonResto[i].description+"','"+jsonResto[i].latitude+"','"+jsonResto[i].longitude+"');init_itineraire("+jsonResto[i].latitude+","+jsonResto[i].longitude+");menu("+i+")\" " +"data-wrapperels=\"div\" data-icon=\"arrow-r\" data-iconpos=\"right\">" +
 						"<div class=\"ui-btn-inner ui-li\"><div class=\"ui-btn-text\"><a class=\"ui-link-inherit\" data-transition=\"slide\">"
 						+ jsonResto[i].nom +"</a></div><span class=\"ui-icon ui-icon-arrow-r ui-icon-shadow\"> </span></div></li>";
 			counter++;
@@ -153,10 +152,13 @@ function makeList(json) {
 	}
 
 }
-function makeaddress(address,code,desc){
+function makeaddress(nom,address,code,desc,lat,lon){
+	nomResto=nom;
 	Radresse=address;
 	Rcode_postal=code;
 	Rdescription=desc;
+	lat=lat;
+	lon=lon;
 }
 // init la liste des resto par ordre alpha
 function initMenuAlpha() {
@@ -186,91 +188,77 @@ function menu(iter) {
 }
 
 function makemenu(json){
-	
-	if(json!=""){
 	html="";
-	var jsonMenu = jQuery.isPlainObject(json) ? json: jQuery.parseJSON(json);
-	var MenuResto = jQuery.isPlainObject(jsonMenu) ? json: jQuery.parseJSON(jsonMenu);
-	var nomResto= jsonMenu.nom;
-	lat= jsonMenu.latitude;
-	//alert("jsonMenu.lat"+lat);
-	lon= jsonMenu.longitude;
-	var EntréesMidi= jsonMenu.menu.midi.Entrées;
-	var PlatsMidi= jsonMenu.menu.midi.Plats;
-	var LégumesMidi= jsonMenu.menu.midi.Légumes;
-	var DessertsMidi= jsonMenu.menu.midi.Desserts;
-	var EntréesSoir= jsonMenu.menu.soir.Entrées;
-	var PlatsSoir= jsonMenu.menu.soir.Plats;
-	var LégumesSoir= jsonMenu.menu.soir.Légumes;
-	var DessertsSoir= jsonMenu.menu.soir.Desserts;
-	var MidiSize = Object.keys(jsonMenu.menu.midi).length;
-	var SoirSize = Object.keys(jsonMenu.menu.soir).length;
-	var today = new Date(); 
-	var serveur= new Date(jsonMenu.date);
-	var day = today.getFullYear()+"-"+today.getUTCMonth()+"-"+today.getDate() ;
-	//var day = today.getFullYear()+"-"+today.getUTCMonth()+"-22" ;
-	var day2 = serveur.getFullYear()+"-"+serveur.getUTCMonth()+"-"+serveur.getDate();	
-		html = "<li><h3>"+nomResto+"</h3></li>";
-		if(day == day2)
-		{
-		if(MidiSize>0)
-			html += "<li id=\"EntréesM\" class=\"midi\"><p><h4>Entrées:</h4>"+EntréesMidi+"</p></li><li id=\"PlatsM\" class=\"midi\"><p><h4>Plats:</h4>"+PlatsMidi+
-			"</p></li><li id=\"LégumesM\" class =\"midi\"data-theme=\"b\"><p><h4>Légumes:</h4>"+ LégumesMidi+
-			"</p></li>	<li id=\"DessertsM\" class = \"midi\" data-theme=\"b\"><p><h4>Desserts:</h4>"+DessertsMidi+" </p></li>";
-		else 
-			html += "<li><p class=\"midi\"><span style='text-decoration:underline' >Midi</span> : Pas de service</p></li>";
-		if(SoirSize>0)
-			html +="<li  id=\"EntréesSoir\" class =\"soir\"><p><h4>Entrées:</h4>"+EntréesSoir+" </p></li><li id=\"Plats\" class =\"soir\"><p><h4>Plats:</h4>"+PlatsSoir+
-			"</p></li><li id=\"Légumes\" class =\"soir\"><p><h4>Légumes:</h4>" +LégumesSoir+"</p></li><li id=\"Desserts\" class = \"soir\"><p><h4>Desserts:</h4>"+DessertsSoir+
-			" </p></li>";
-		else 
-			html += "<li><p class=\"soir\"><span style='text-decoration:underline' ><h4>Soir</span> : Pas de service</h4></p></li>";
-		$('#listebeta').html(html);	
-		$('#listeAlpha').hide();
-		$('#page_footer').hide();
-		$("#listebeta").show();
-		$('#itineraire').show();
-		$("#mode").show();
-		$("#btnBack").show();
-		
-		}
-		else {
-			alert("le menu n'a pas été envoyer!!!");
-			$('#listeAlpha').hide();
-			$('#page_footer').hide();
-			$("#listebeta").show();
-			$('#itineraire').show();
-			$("#mode").show();
-			$("#btnBack").show();
-			$('#listebeta').html(html);	
-			
-		}
-	setEnd(lat,lon);
+	html="<li><h3>"+nomResto+"</h3></li>";
+	if(json!="")
+	{
+		var jsonMenu = jQuery.isPlainObject(json) ? json: jQuery.parseJSON(json);
+		var MenuResto = jQuery.isPlainObject(jsonMenu) ? json: jQuery.parseJSON(jsonMenu);		
+		lat= jsonMenu.latitude;
+		//alert("here3:"+lat);
+		lon= jsonMenu.longitude;
+		var EntréesMidi= jsonMenu.menu.midi.Entrées;
+		var PlatsMidi= jsonMenu.menu.midi.Plats;
+		var LégumesMidi= jsonMenu.menu.midi.Légumes;
+		var DessertsMidi= jsonMenu.menu.midi.Desserts;
+		var EntréesSoir= jsonMenu.menu.soir.Entrées;
+		var PlatsSoir= jsonMenu.menu.soir.Plats;
+		var LégumesSoir= jsonMenu.menu.soir.Légumes;
+		var DessertsSoir= jsonMenu.menu.soir.Desserts;
+		var MidiSize = Object.keys(jsonMenu.menu.midi).length;
+		var SoirSize = Object.keys(jsonMenu.menu.soir).length;
+		var today = new Date(); 
+		var serveur= new Date(jsonMenu.date);
+		var day = today.getFullYear()+"-"+today.getUTCMonth()+"-"+today.getDate() ;
+		//var day = today.getFullYear()+"-"+today.getUTCMonth()+"-22" ;
+		var day2 = serveur.getFullYear()+"-"+serveur.getUTCMonth()+"-"+serveur.getDate();	
+			if(day == day2)
+			{
+				if(MidiSize>0)
+					html += "<li id=\"EntréesM\" class=\"midi\"><p><h4>Entrées:</h4>"+EntréesMidi+"</p></li><li id=\"PlatsM\" class=\"midi\"><p><h4>Plats:</h4>"+PlatsMidi+
+					"</p></li><li id=\"LégumesM\" class =\"midi\"data-theme=\"b\"><p><h4>Légumes:</h4>"+ LégumesMidi+
+					"</p></li>	<li id=\"DessertsM\" class = \"midi\" data-theme=\"b\"><p><h4>Desserts:</h4>"+DessertsMidi+" </p></li>";
+				else 
+					html += "<li><p class=\"midi\"><span style='text-decoration:underline' >Midi</span> : Pas de service</p></li>";
+				if(SoirSize>0)
+					html +="<li  id=\"EntréesSoir\" class =\"soir\"><p><h4>Entrées:</h4>"+EntréesSoir+" </p></li><li id=\"Plats\" class =\"soir\"><p><h4>Plats:</h4>"+PlatsSoir+
+					"</p></li><li id=\"Légumes\" class =\"soir\"><p><h4>Légumes:</h4>" +LégumesSoir+"</p></li><li id=\"Desserts\" class = \"soir\"><p><h4>Desserts:</h4>"+DessertsSoir+
+					" </p></li>";
+				else 
+					html += "<li><p class=\"soir\"><span style='text-decoration:underline' ><h4>Soir</span> : Pas de service</h4></p></li>";
+			}
+			else {
+				alert("le menu n'a pas été envoyer!!!");		
+			}
+		//setEnd(lat,lon);
 	
 	}
 	else {
 		alert("closed");
 	}
 	html+="<li><h4>adresse:"+Radresse+","+Rcode_postal+","+Rdescription+"</h4></li>";
+	$('#listebeta').html(html);	
 	$('#listeAlpha').hide();
 	$('#page_footer').hide();
 	$("#listebeta").show();
 	$('#itineraire').show();
 	$("#mode").show();
 	$("#btnBack").show();
-	$('#listebeta').html(html);	
-	
-}
 
+}
+/*
 function setEnd(lat,lon){
 	//alert("here in setEnd"+lat+"lon:"+lon);
 	end = new google.maps.LatLng(lat,lon);
+	alert("here5:"+lat);
 	//alert("end="+end);
 	$('#end').attr('value', end);
-}
+}*/
 //btn retour sur la liste des resto qd on est ds le menu
 $(document).on('click','#btnBack', function(){ 
 								initMenuAlpha();
+								html="";
+								$('#EmplacementItineraireTexte').html(html);	
 								$("#btnBack").hide();					
 								$("#itineraire").hide();	
 								$("#mode").hide();
